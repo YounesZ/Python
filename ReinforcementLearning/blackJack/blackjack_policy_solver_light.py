dcf336764ce65ccb2c51af3f815b7f290ea58d0c
YounesZ <younesz@stradigi.ca>
2017-10-19 17:16:10 -0400

    Blackjack MC policy solver: ES finished, soft e-greedy finished, launched comparison of the two

--- ReinforcementLearning/blackJack/blackjack_policy_solver_light.py
+++ ReinforcementLearning/blackJack/blackjack_policy_solver_light.py
@@ -3,24 +3,32 @@ from random import choice, random
 from copy import deepcopy
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import pickle
 import time
 from mpl_toolkits.mplot3d import Axes3D
 
-class blackjack_policy_solver2():
+class blackjack_policy_solver_light():
 
-    def __init__(self, initMode='random', gameType='finite'):
+    def __init__(self, initMode='random', gameType='finite', eGreedy=0):
         # States
         self.agent_states   =   list( range(12,22) )
         self.dealer_states  =   list( range(2,12) )
         # Policies
         self.policy_dealer  =   [True]*16 + [False]*5
         if initMode=='random':
-            self.policy_agent   =   np.random.random([len(self.agent_states), len(self.dealer_states), 2])>0.5
+            self.policy_agent   =   np.random.random([len(self.agent_states), len(self.dealer_states), 2])
         elif initMode=='stick20':
             self.policy_agent   =   np.concatenate( (np.ones([8, 10, 2]), np.zeros([2, 10, 2])), axis=0 )
         # Card probabilities
         self.gameType       =   gameType
         self.card_reset()
+        # Values
+        self.returns        =   np.zeros([len(self.agent_states), len(self.dealer_states), 2, 2])
+        self.visits         =   np.ones([len(self.agent_states), len(self.dealer_states), 2, 2])
+        self.action_value   =   np.zeros([len(self.agent_states), len(self.dealer_states), 2, 2])
+        self.state_value    =   np.zeros([len(self.agent_states), len(self.dealer_states), 2])
+        # Agent greediness
+        self.eGreedy        =   eGreedy
 
     def card_reset(self):
         self.cardProba      =   [1/13]*8 + [4/13] + [1/13]
@@ -38,6 +46,26 @@ class blackjack_policy_solver2():
             self.cardProba[idCard-2]    -=  1/52
         return idCard
 
+    def greedy_choice(self, value):
+        # Find the max
+        nEl     =   len(value)
+        iMax    =   np.argmax(value)
+        # Greedy value
+        if iMax ==  0:
+            greedV      =   self.eGreedy/nEl
+        else:
+            greedV      =   1 - self.eGreedy * (1 - 1/nEl)
+        return greedV
+
+    def greedy_action(self, method='random'):
+        if method=='random':
+            action  =   choice([0,1])
+        elif method=='soft_egreedy':
+            action  =   self.policy_agent[self.agent['state']-12, self.dealer['state']-2, int(self.agent['usable'])]
+            actionP =   np.multiply([action, 1-action], np.random.random([1,2]))
+            action  =   np.argmax(actionP)==0
+        return action
+
     def init_episode(self):
         self.card_reset()
         # Choose 1 random state for agent
@@ -53,7 +81,20 @@ class blackjack_policy_solver2():
         # Choose 1 random state for dealer
         self.dealer =   {'state':self.card_draw(choice(self.dealer_states)), 'usable':0}
 
-    def run_episode(self):
+    def init_episode2(self):
+        self.card_reset()
+        # Draw cards for Agent
+        self.agent  =   {'state':0, 'usable':False}
+        while self.agent['state']<12:
+            card    =   self.card_draw()
+            if card ==  11 and self.agent['state']==11:
+                card-=  10
+            self.agent['state']     +=  card
+            self.agent['usable']    =   self.agent['usable'] + card==1
+        # Draw card for Dealer
+        self.dealer = {'state': self.card_draw(), 'usable': False}
+
+    def run_MCES_episode(self):
         # Init variables
         state_chain     =   [[self.agent['state'], self.dealer['state']]]
         action_chain    =   []
@@ -62,7 +103,11 @@ class blackjack_policy_solver2():
 
         # Agent's turn
         loopon  =   True
-        action  =   choice([0,1])
+        if self.eGreedy==0:
+            # If no softness on policy, need exploratory starts
+            action  =   self.greedy_action(method='random')
+        else:
+            action  =   self.greedy_action(method='soft_egreedy')
         while loopon:
             # HIT
             if action:
@@ -77,9 +122,10 @@ class blackjack_policy_solver2():
                     reward  =   -1
                     loopon  =   False
                 if self.agent['state'] <= 21:
-                    action      =   deepcopy(self.policy_agent[self.agent['state']-12, self.dealer['state']-2, self.agent['usable']])
+                    # Select action
+                    action  =   self.greedy_action(method='soft_egreedy')
                     state_chain.append([self.agent['state'], self.dealer['state']])
-                    usable_chain.append(int(bool(self.agent['usable'])))
+                    usable_chain.append(int(self.agent['usable']))
             else: # STICK
                 action_chain.append(0)
                 loopon =    False
@@ -117,33 +163,39 @@ class blackjack_policy_solver2():
         return reward, state_chain, action_chain, usable_chain
 
     def monte_carlo_ES(self, nIterations):
-        # Init variables
-        returns     =   np.zeros([len(self.agent_states), len(self.dealer_states), 2, 2])
-        visits      =   np.ones([len(self.agent_states), len(self.dealer_states), 2, 2])
         # Loop
         for ii in range(nIterations):
-            if not ii%5000: print('Iteration '+str(ii))
+            #if not ii%5000: print('Iteration '+str(ii))
             # Init episode
             self.init_episode()
             # Run the episode
-            reward, state_chain, action_chain, usable_chain     =   self.run_episode()
+            reward, state_chain, action_chain, usable_chain     =   self.run_MCES_episode()
             #print(state_chain)
             #print('actions', action_chain)
             #print('usable', usable_chain)
             #print('reward', reward)
             # Distribute reward
             for st, ac, us in zip(state_chain, action_chain, usable_chain):
-                returns[st[0]-12, st[1]-2, us, ac]  +=  reward
-                visits[st[0]-12, st[1]-2, us, ac]   +=  1
-            # State value and policy
-            self.action_value   =   returns / visits
-            self.policy_agent   =   np.argmax( self.action_value, axis=3 )
-            self.state_value    =   np.max( self.action_value, axis=3 )
+                self.returns[st[0]-12, st[1]-2, us, ac]  +=  reward
+                self.visits[st[0]-12, st[1]-2, us, ac]   +=  1
+                # State value and policy
+                self.action_value[st[0] - 12, st[1] - 2, us, ac]    =   self.returns[st[0] - 12, st[1] - 2, us, ac] / self.visits[st[0] - 12, st[1] - 2, us, ac]
+                self.policy_agent[st[0] - 12, st[1] - 2, us]        =   self.greedy_choice(self.action_value[st[0] - 12, st[1] - 2, us, :])
+                self.state_value[st[0] - 12, st[1] - 2, us]         =   np.max(self.action_value[st[0] - 12, st[1] - 2, us, :])
 
-    def print_policy(self):
+    def data_save(self, filename):
+        # Package the data
+        dataPack    =   {'policy':self.policy_agent, 'stateValues':self.state_value}
+        # Save
+        with open(filename, "wb") as f:
+            pickle.dump(dataPack, f, pickle.HIGHEST_PROTOCOL)
+
+    def print_policy(self, Policy=None):
         fig     =   plt.figure()
         # Axis 1: unusable ace
-        Z       =   self.policy_agent[:, :, 0]
+        Z       =   Policy
+        if Policy is None:
+            Z   =   self.policy_agent[:, :, 0]
         ax1     =   fig.add_subplot(121)
         surf1   =   ax1.imshow(Z)
         ax1.set_xlabel("Dealer's states")
@@ -168,11 +220,14 @@ class blackjack_policy_solver2():
         ax2.set_yticklabels([self.agent_states[x] for x in range(0, len(self.agent_states), 2)])
         ax2.invert_yaxis()
 
-    def print_stateValue(self):
+    def print_stateValue(self, StateV=None):
         fig     =   plt.figure()
         X, Y    =   np.meshgrid(self.agent_states, np.flipud(self.dealer_states))
         # Axis 1: usable ace
-        Z       =   np.transpose(np.reshape(self.state_value[:,:,1], [len(self.agent_states), len(self.dealer_states)]))
+        if StateV is None:
+            Z   =   self.state_value[:,:,1]
+        else:
+            Z   =   StateV[:, :, 1]
         ax1     =   fig.add_subplot(121, projection='3d')
         surf1   =   ax1.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
         ax1.set_xlim(self.agent_states[0], self.agent_states[-1])
@@ -185,7 +240,10 @@ class blackjack_policy_solver2():
         ax1.set_title('Usable ace')
         ax1.invert_yaxis()
         # Axis 2: unusable ace
-        Z       =   np.transpose(np.reshape(self.state_value[:,:,0], [len(self.agent_states), len(self.dealer_states)]))
+        if StateV is None:
+            Z   =   self.state_value[:,:,0]
+        else:
+            Z   =   StateV[:, :, 0]
         ax2     =   fig.add_subplot(122, projection='3d')
         surf2   =   ax2.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
         ax2.set_xlim(self.agent_states[0], self.agent_states[-1])
@@ -201,10 +259,11 @@ class blackjack_policy_solver2():
 
 
 # ==== LAUNCHER
-BJS    =   blackjack_policy_solver2(initMode='random')
-BJS.monte_carlo_ES(500000)
-BJS.print_policy()
-BJS.print_stateValue()
+#BJS    =   blackjack_policy_solver_light(initMode='random', eGreedy=0.0)
+#BJS.monte_carlo_ES(500000)
+#BJS.data_save('ReinforcementLearning/blackJack/blackjack_MCES_solution.p')
+#BJS.print_policy()
+#BJS.print_stateValue()
 
 
 
---
+++ ReinforcementLearning/blackJack/script_blackjack_compare_learning.py
@@ -0,0 +1,49 @@
+""" This script implements a comparison scheme between different types of learning
+    Each learning algorithm is ran several times """
+
+import pickle
+import numpy as np
+from ReinforcementLearning.blackJack.blackjack_policy_solver_light import *
+
+
+# Set variables
+learningTypes   =   ['egreedy0', 'egreedy0.05', 'egreedy0.1']
+iterVec         =   range(0, 500000, 10000)
+iterSteps       =   [0]+[2000]*50
+nIterations     =   50
+svFile          =   'results_compare_learning_egreedy_500K'
+
+# Load ground truth
+groundT         =   pickle.load( open('ReinforcementLearning/blackJack/blackjack_solution.p', 'rb') )['policy']
+
+# Set empty containers
+MSE     =   np.zeros([nIterations, len(iterSteps), len(learningTypes)])
+
+print('Launching learning evaluation script for 3 type of learning:')
+# Loop on number of iterations
+for nIt in range(nIterations):
+
+    print('\t***Iteration: ', str(nIt))
+    # Initialize the solvers
+    solver_eg0      =   blackjack_policy_solver_light(initMode='random', eGreedy=0.0)
+    solver_eg005    =   blackjack_policy_solver_light(initMode='random', eGreedy=0.05)
+    solver_eg01     =   blackjack_policy_solver_light(initMode='random', eGreedy=0.1)
+
+    for nSt in range(len(iterSteps)):
+
+        print('\t\tSolution step: ', str(nSt))
+        # Solve for policy
+        solver_eg0.monte_carlo_ES(iterSteps[nSt])
+        solver_eg005.monte_carlo_ES(iterSteps[nSt])
+        solver_eg01.monte_carlo_ES(iterSteps[nSt])
+
+        # Compute MSE
+        MSE[nIt, nSt, 0]    =   np.sqrt(np.sum((solver_eg0.policy_agent - groundT)**2))
+        MSE[nIt, nSt, 1]    =   np.sqrt(np.sum((solver_eg005.policy_agent - groundT) ** 2))
+        MSE[nIt, nSt, 2]    =   np.sqrt(np.sum((solver_eg01.policy_agent - groundT) ** 2))
+
+
+# Save to file
+pack    =   {'lerningTypes':learningTypes, 'iterSteps':iterSteps, 'groundT':groundT, 'MSE':MSE}
+pickle.dump( pack, open('ReinforcementLearning/blackJack/'+svFile+'.p', 'wb') )
+
