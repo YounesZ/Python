b2c3f1ce04a112feb7058301dc574e5a4efde33b
YounesZ <younesz@stradigi.ca>
2018-02-03 00:32:12 -0500

    NHL: small debugging

---
+++ Utils/programming/ut_clone_directory.py
@@ -0,0 +1,25 @@
+import shutil
+import errno
+from os import path
+
+
+def ut_clone_directory(src, dst):
+
+    try:
+        shutil.copytree(src, dst)
+    except OSError as e:
+        # If the error was caused because the source wasn't a directory
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            print('Directory not copied. Error: %s' % e)
+
+
+
+# ========
+# LAUNCHER
+"""
+src     =   '/home/younesz/Documents/Code/NHL_stats_SL/ReinforcementLearning/NHL/playerstats/offVSdef/Automatic_classification/MODEL_perceptron_1layer_10units_relu'
+dst     =   path.join( src.replace(path.basename(src), ''), 'MODELS', 'MODEL_perceptron_1layer_10units_relu_LOO_SeasonXXX' )
+ut_clone_directory(src, dst)
+"""
